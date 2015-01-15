2a11ae480cefe5a96c83a0091c9354326ddd8dca
Trent <TrentDensley@gmail.com>
2015-01-15 16:52:27 -0700

    add

---
+++ app.js
@@ -0,0 +1,45 @@
+
+
+$(document).ready(function() {
+
+	var color = 'white';
+	var colors = 'red blue green yellow white';
+
+
+	$('.box').on('click', function() {
+    	$(this).addClass(color);
+
+  });
+
+	$('.box').on('dblclick', function() {
+   		$(this).removeClass(colors);
+   
+   });
+
+	$('#reset').on('click', function() {
+		$('.box').removeClass(colors);
+	});
+
+
+
+$('#red').on('click', function() {
+	color = 'red';
+});
+
+$('#blue').on('click', function() {
+	color = 'blue';
+});
+
+$('#green').on('click', function() {
+	color = 'green';
+});
+
+$('#yellow').on('click', function() {
+	color = 'yellow';
+});
+
+$('#white').on('click', function() {
+	color = 'white';
+});
+
+  });
